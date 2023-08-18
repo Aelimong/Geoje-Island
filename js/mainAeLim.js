3018b9285b165980463d5041647f0ae7740c84ac
Aelimong <dofla0507@gmail.com>
2023-08-18 11:26:49 +0900

    feat(AeLim): 메인페이지 거제도 소개, 메뉴 구현

--- js/mainAeLim.js
+++ js/mainAeLim.js
@@ -1,4 +1,4 @@
-//현재 시간 불러오기
+//현재 시간 불러오기 =====================================
 let timestamp = new Date().getTime();
 
 // timestamp Date 객체로 변환 출력
@@ -114,14 +114,25 @@ function changeTicketImages(MM) {
 changeTicketImages(MM);
 
 /**
- * 티켓 클릭시 계절별 페이지로 이동
+ * 티켓 클릭시 애니메이션 적용 후 계절별 페이지로 이동
  */
 
 let ticketInfo = document.querySelector(".ticket-info");
 let ticketFlower = document.querySelector(".ticket-flower");
 
-ticketInfo.addEventListener("click", moveFourSeosonPage);
-ticketFlower.addEventListener("click", moveFourSeosonPage);
+ticketInfo.addEventListener("click", addPaperAnimation);
+ticketFlower.addEventListener("click", addPaperAnimation);
+
+function addPaperAnimation() {
+  //ticketInfo, ticketFlower 애니메이션 클래스 적용
+  ticketInfo.classList.toggle("rotate-10-left");
+  ticketFlower.classList.toggle("rotate-10-right");
+
+  // 1.5초뒤 페이지 이동
+  setTimeout(() => {
+    moveFourSeosonPage();
+  }, 1500);
+}
 
 function moveFourSeosonPage() {
   let month = Number(MM);
@@ -146,4 +157,94 @@ function moveFourSeosonPage() {
   } else {
     location.href = winterUrl;
   }
+}
+
+// 메인섹션에서는 메뉴 안 보이도록 구현 =====================================
+let mainMenu = document.querySelector(".main-menu");
+let mainSection = document.querySelector(".main-section");
+let mainSecHeight = mainSection.clientHeight;
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY <= mainSecHeight - 80) {
+    mainMenu.classList.add("menu-hide");
+  } else {
+    mainMenu.classList.remove("menu-hide");
+  }
+});
+
+// 스크롤 감지 메뉴 구현 =====================================
+let didScroll;
+let lastScrollTop = 0;
+let delta = 5;
+let navbarHeight = $("header.main-menu").outerHeight();
+
+// 스크롤시에 사용자가 스크롤했다는 것을 알림
+$(window).scroll(function (event) {
+  didScroll = true;
+});
+
+// hasScrolled()를 실행하고 didScroll 상태를 재설정
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  let st = $(this).scrollTop();
+
+  if (Math.abs(lastScrollTop - st) <= delta) return;
+
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    $("header.main-menu").removeClass("nav-down").addClass("nav-up");
+  } else {
+    // Scroll Up
+    if (st + $(window).height() < $(document).height()) {
+      $("header.main-menu").removeClass("nav-up").addClass("nav-down");
+    }
+  }
+
+  lastScrollTop = st;
+}
+
+// 메인 전체메뉴 보이기 =====================================
+function showNav() {
+  let mainNav = document.querySelector(".main-nav");
+  let navArea = document.querySelector(".nav-area");
+  let bodyHeight = document.body.scrollHeight;
+  let viewHeight = window.innerHeight;
+
+  if (mainNav.classList.contains("fade-out-box")) {
+    mainNav.classList.remove("fade-out-box");
+    mainNav.classList.add("fade-in-box");
+  } else {
+    mainNav.classList.add("fade-in-box");
+  }
+
+  mainNav.setAttribute("style", `display: block; height: ${bodyHeight}px`);
+  navArea.setAttribute("style", `height: calc(${viewHeight}px - 80px)`);
+}
+
+// 메인 전체메뉴 숨기기 =====================================
+function hideNav() {
+  let mainNav = document.querySelector(".main-nav");
+
+  if (mainNav.classList.contains("fade-in-box")) {
+    mainNav.classList.remove("fade-in-box");
+    mainNav.classList.add("fade-out-box");
+  } else {
+    mainNav.classList.add("fade-out-box");
+  }
+
+  setTimeout(() => {
+    mainNav.style.display = "none";
+  }, 500);
+}
+
+//페이지 새로고침 될 때 전체 메뉴 숨기기
+const entries = performance.getEntriesByType("navigation")[0];
+if (entries.type === "reload") {
+  hideNav();
 }
