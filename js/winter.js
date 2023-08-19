96585068d17f27580c6a4d97d5ddab4a0573c433
yoochangyong <changd0302@gmail.com>
2023-08-19 14:05:18 +0900

    feat(winter-inging)(ChangYong)

--- js/winter.js
+++ js/winter.js
@@ -1,9 +1,24 @@
-const swiper = new swiper(".carousel-inner", {
-  direction: "vertical",
-  loop: false,
-  speed: 1000,
-  mousewheel: true,
+const sw = new Swiper(".swiper1", {
+  // direction: "vertical",
+  slidesPerView: 3,
+  spaceBetween: 6, //slide 간의 gap (margin)
+  loop: true,
+  autoplay: {
+    delay: 1000,
+  },
+  speed: 1000, //like transition
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
   scrollbar: {
     el: ".swiper-scrollbar",
   },
 });
+
+console.log("js");
