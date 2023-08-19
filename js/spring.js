ae7fd129239e9e40670e3ad1b2205cc1c80e7179
mameruu <gkswjddbs95@naver.com>
2023-08-19 19:45:09 +0900

    feat(JeongYun):슬라이스 섹션 작업중

--- js/spring.js
+++ js/spring.js
@@ -5,4 +5,27 @@ new Swiper(".swiper", {
   loop: true,
   slidesPerView: 2,
   spaceBetween: 10,
+  speed: 1000,
+  loopedSlides: 1,
+  // loopAdditionalSlides: 1,
 });
+
+// const { gsap } = window;
+// function initScrollAnimations() {
+//   const elementsToAnimate = document.querySelectorAll(
+//     ".ygPage, .ggPage, .hdPage, .food"
+//   );
+
+//   elementsToAnimate.forEach((element) => {
+//     gsap.from(element, {
+//       opacity: 0,
+//       y: 50,
+//       duration: 1,
+//       scrollTrigger: {
+//         trigger: element,
+//         start: "top 80%",
+//         end: "bottom 20%",
+//         toggleActions: "play none none reverse",
+//       },
+//     });
+//   });
