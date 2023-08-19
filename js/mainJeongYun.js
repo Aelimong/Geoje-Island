de61757bf20d5d014e30eb84eb920f1ea730e06e
mameruuu <gkswjddbs95@naver.com>
2023-08-20 03:32:21 +0900

    feat(JeongYun):main화면  애니메이션 추가

--- js/mainJeongYun.js
+++ js/mainJeongYun.js
@@ -1,42 +1,80 @@
-const ment = document.getElementById("ment");
-const move = document.getElementById("move");
-const slider = document.getElementById("slider");
-const s_wid = slider.offsetWidth;
-const sLi = slider.children;
-let win_wid = window.innerWidth;
-let s_move_max = (s_wid - win_wid / 2) * -1;
-let s_pos = 0;
-let li_pos = 0;
-let pct = 0;
+//로딩페이지
+const max = 100;
 
-ment.addEventListener("wheel", function (e) {
-  e.preventDefault;
-  if (e.deltaY > 0) {
-    move.style.top = `0%`;
-  }
-});
+function counter(max) {
+  document.body.classList.add("overflow-hidden");
+  let now = max;
 
-move.addEventListener("wheel", function (e) {
-  e.preventDefault;
-  if (e.deltaY < 0 && s_pos >= 0) {
-    setTimeout(() => {
-      move.style.top = `100%`;
-    }, 500);
-    return;
-  }
-  move_slider(e.deltaY);
-  on_indicator();
-});
+  const handle = setInterval(() => {
+    document.querySelector(".count").innerHTML = Math.ceil(max - now);
 
-function move_slider(amount) {
-  s_pos -= amount;
-  if (s_pos < s_move_max) {
-    s_pos = s_move_max;
-    return;
-  } else if (s_pos > 0) {
-    s_pos = 0;
-    return;
-  }
-  slider.style.transform = `translateX(${s_pos}px)`;
-  li_upDown(amount);
+    // 목표에 도달하면 정지,
+    //   정지 후의 코드는 아래에 작성
+    if (now < 0.5) {
+      clearInterval(handle);
+      // 숫자 정지 후
+      /* 
+      body의 overflow 없앰으로써 스크롤 제거
+      두번째 section인 main클래스 보이도록
+      svg 에 horizontal 클래스 추가해서 비행기 svg 색변경 & 수평(rotate) 맞추기
+      */
+      document.body.classList.remove("overflow-hidden");
+      document.querySelector(".main").classList.add("show");
+      document.querySelector(".svg").classList.add("horizontal");
+      return 0;
+    }
+
+    // 적용될 수치, 점점 줄어듬
+    let step = now / 10;
+
+    //   99일 때 count 천천히 되도록
+    if (now < 2) {
+      step = now / 20;
+    }
+    now -= step;
+  }, 50);
 }
+//   로드 되었을 때 로딩 변경 되도록
+window.addEventListener("load", () => {
+  counter(100);
+});
+
+// 2번째 섹션 가로스크롤
+let tl = gsap.timeline();
+tl.to("#move", {
+  xPercent: -50, // 기본 위치에서 왼쪽으로 40% 만큼 이동 (스크롤 내릴 때 section을 가로로 움직이도록)
+});
+// 스크롤 Trigger 동작
+ScrollTrigger.create({
+  animation: tl, // 위에서 만들어준 #move에 대한 타임라인
+  trigger: "#move", // (move 아이디를 가진 두번째 section을 만나면 gsap 발동)
+  start: "top top", // section을 기준으로 맨 위에서 시작
+  end: "bottom bottom", // 끝점 설정
+  markers: true, // 배포시 markers 옵션은 false 로 변경해주세요
+  scrub: 1, // 부드러운 움직임 + 되돌아갈 때도 스크롤 기반 애니메이션 동작하도록
+  pin: true, // #move 에서 애니메이션이 동작할 때는 고정된 위치에 있도록
+  end: "+=2800", // 3000px 에 해당하는 스크롤 범위만큼 애니메이션이 동작하도록
+});
+
+// 2번째 섹션 나타나기
+function scrollAnimation() {
+  const elemtMoving = document.querySelectorAll("#move");
+
+  elemtMoving.forEach((element) => {
+    gsap.from(element, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+
+      scrollTrigger: {
+        trigger: element,
+        start: "top 60%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
+}
+
+// addEventListener를 까먹지 맙시다 .,.
+document.addEventListener("DOMContentLoaded", scrollAnimation);
