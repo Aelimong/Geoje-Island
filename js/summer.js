0015f42caff08573e4501132d5d34ed1858360e0
Aelimong <dofla0507@gmail.com>
2023-08-20 19:24:23 +0900

    feat(AeLim): 여름 페이지 사진 변경, page 올라오는 애니메이션 추가

--- js/summer.js
+++ js/summer.js
@@ -6,7 +6,6 @@ const buttons = document.querySelectorAll(".mainButton");
 
 function hoverIn(event) {
   gsap.to(event.target, { opacity: 0.65, duration: 0.3 });
-  console.log(event.target.classList.contains(`pin4`));
   for (let i = 0; i < 4; i++) {
     if (event.target.classList.contains(`pin${i + 1}`)) {
       gsap.to(`.bt${i + 1}`, { opacity: 0.65, duration: 0.3 });
@@ -88,3 +87,30 @@ const swiper = new Swiper(".swiper", {
     },
   },
 });
+
+//page3 ~ page5
+// 스크롤에 따라 부드럽게 요소들이 올라온다
+
+function scrollAnimation() {
+  const elemtMoving = document.querySelectorAll(
+    ".page3, .page4, .page5, .page7, .page8"
+  );
+
+  elemtMoving.forEach((element) => {
+    gsap.from(element, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      // ease: "ease-in-out",
+      scrollTrigger: {
+        trigger: element,
+        start: "top 80%",
+        end: "bottom 20%",
+        toggleActions: "play none none reverse",
+      },
+    });
+  });
+}
+
+//초기화
+document.addEventListener("DOMContentLoaded", scrollAnimation);
